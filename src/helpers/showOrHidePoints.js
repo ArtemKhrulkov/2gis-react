0021c38192fa4deb99de05dcbd3814bd4c478c24
ArtemKhrulkov <artemhrulkov@gmail.com>
2020-09-19 16:20:55 +0500

    Update showOrHidePoints.js

--- src/helpers/showOrHidePoints.js
+++ src/helpers/showOrHidePoints.js
@@ -1,5 +1,5 @@
 export const showOrHidePoints = (params) => {
-  if (params.length < 4) {
+  if (params.length <= 4) {
     params[0].show();
   } else {
     params.forEach((p, idx) => idx !== 0 && p.hide())
